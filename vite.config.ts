a1735841d0fa4a2eb73bba293689ca346fd8038d
samuelatuma <samuel.atuma@flutterwavego.com>
2025-11-04 19:31:16 +0100

    work

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-// vite.config.ts
-
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite"; 
+import { vercelPreset } from "@vercel/remix/vite";
 
-installGlobals(); 
+installGlobals({ nativeFetch: true });
 
 export default defineConfig({
   plugins: [
     remix({
-      presets: [vercelPreset()], 
-      
+      presets: [vercelPreset()],
+      ignoredRouteFiles: ["**/.*"],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+      },
     }),
-    
-    (tsconfigPaths as typeof tsconfigPaths)(),
+    tsconfigPaths(),
   ],
+  build: {
+    assetsInlineLimit: 0,
+  },
 }) satisfies UserConfig;
